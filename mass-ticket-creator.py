5400b8b8dddba4c261ca045fecaec2c84836596e
ullahhab <hammadullahris@gmail.com>
2026-04-14 03:17:42 -0500

    authentication mechanism and saving to file

--- mass-ticket-creator.py
+++ mass-ticket-creator.py
@@ -4,6 +4,19 @@ import json
 import os
 import sys
 from dotenv import load_dotenv
+from pathlib import Path
+
+def check_connection():
+    global headers, baseurl, auth
+    url = f'{baseurl}/rest/api/3/users/search'
+    
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        auth=auth
+    )
+    return response.status_code==200
 
 def getallProject():
     global headers, baseurl, auth
@@ -109,21 +122,41 @@ def getAllIssueTypes():
     )
     print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
 
-if __name__ == "__main__":
-    if ".env" not in os.listdir():
-        print(".env doesn't exist. Please check if you're running from same location as .env or create a env with following config:\n APITOKEN=<TOKEN>\nbaseURL=<baseurl>\nemail=<your email>") 
-        sys.exit(1)
-    load_dotenv()
-    api_token = os.getenv("APITOKEN")
-    baseurl= os.getenv("baseURL")
-    email = os.getenv("email")
+def check_file_exist():
+    script_dir = Path(__file__).resolve().parent
+    return os.path.exists(f"{script_dir}/.env")
 
+def write_to_file():
+    global api_token, baseurl, email, script_dir
+    file = open(f"{script_dir}/.env", 'w')
+    file.write(f"APITOKEN={api_token}\nbaseURL={baseurl}\nemail={email}")
+    file.close()
+
+if __name__ == "__main__":
+    script_dir =  Path(__file__).resolve().parent
+    if not check_file_exist():
+        api_token = input("Please type api token: ")
+        baseurl = input("please type base url: ")
+        email = input("please type email: ")
+    else:
+        load_dotenv()
+        api_token = os.getenv("APITOKEN")
+        baseurl= os.getenv("baseURL")
+        email = os.getenv("email")
     auth = HTTPBasicAuth(email, api_token)
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json"
     }
-    with open('command.json', 'r') as file:
+    while not check_connection():
+        api_token = input("Please type api token: ")
+        baseurl = input("please type base url: ")
+        email = input("please type email: ")
+        auth = HTTPBasicAuth(email, api_token)
+        
+    write_to_file()
+    with open(f'{script_dir}/command.json', 'r') as file:
+        
         command = json.load(file)
     createJiraTickets(command)
     #getallProject()
